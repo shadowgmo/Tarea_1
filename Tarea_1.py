7434a903ac822ed33ddf749646109ab0298778a1
shadowgmo <lgcastro15@gmail.com>
2019-04-02 14:24:22 -0600

    Add files via upload

---
+++ Tarea_1.py
@@ -0,0 +1,8 @@
+print ('test')
+
+import math
+pi = math.pi
+
+stringvalue = pi
+print ('* %.9s' % (stringvalue), '** %.8s ' % (stringvalue), '*** %.7s ' % (stringvalue),'**** %.6s ' % (stringvalue), '***** %.5s' % (stringvalue),'****** %.4s' % (stringvalue))
+
